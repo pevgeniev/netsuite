7e05594f7aedb83204fed5e713cf654db4e15fdf
pevgeniev <pevgeniev@gmail.com>
2025-01-31 08:21:19 +0200

    add some cors options

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -17,10 +17,11 @@ export class AuthService {
     showDebugInformation: true,
     strictDiscoveryDocumentValidation: false,
     useSilentRefresh: true,
+    checkOrigin: false,
     
     issuer: 'https://4678272.app.netsuite.com', // NetSuite OIDC issuer URL
-    //tokenEndpoint: 'https://4678272.suitetalk.api.netsuite.com/services/rest/auth/oauth2/v1/token', // Token endpoint from NetSuite
-    //userinfoEndpoint: 'https://4678272.suitetalk.api.netsuite.com/services/rest/auth/oauth2/v1/userinfo', // Userinfo endpoint from NetSuite
+    tokenEndpoint: 'https://4678272.suitetalk.api.netsuite.com/services/rest/auth/oauth2/v1/token', // Token endpoint from NetSuite
+    userinfoEndpoint: 'https://4678272.suitetalk.api.netsuite.com/services/rest/auth/oauth2/v1/userinfo', // Userinfo endpoint from NetSuite
     clientId: '7a0a3c441c3cf1ff3898e6b92de44e00ae386952880cb21c0249d93e6b718540',
     redirectUri: 'https://pevgeniev.github.io/netsuite/callback', // Callback URL after authentication
   };
@@ -28,7 +29,9 @@ export class AuthService {
   initAuth() {
     this.oauthService.configure(this.authConfig);
     this.oauthService.setupAutomaticSilentRefresh();
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    this.oauthService.checkOrigin = false;
+    this.oauthService.tryLogin();
+    
   }
 
   login() {
