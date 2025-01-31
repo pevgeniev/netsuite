0f552a02b3aff38bd266c8439c988d397db56881
pevgeniev <pevgeniev@gmail.com>
2025-01-31 08:26:56 +0200

    set login and logout url

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -18,10 +18,12 @@ export class AuthService {
     strictDiscoveryDocumentValidation: false,
     useSilentRefresh: true,
     checkOrigin: false,
-    
+
     issuer: 'https://4678272.app.netsuite.com', // NetSuite OIDC issuer URL
     tokenEndpoint: 'https://4678272.suitetalk.api.netsuite.com/services/rest/auth/oauth2/v1/token', // Token endpoint from NetSuite
     userinfoEndpoint: 'https://4678272.suitetalk.api.netsuite.com/services/rest/auth/oauth2/v1/userinfo', // Userinfo endpoint from NetSuite
+    loginUrl: 'https://4678272.app.netsuite.com/app/login/oauth2/authorize.nl',
+    logoutUrl: 'https://4678272.suitetalk.api.netsuite.com/services/rest/auth/oauth2/v1/logout',
     clientId: '7a0a3c441c3cf1ff3898e6b92de44e00ae386952880cb21c0249d93e6b718540',
     redirectUri: 'https://pevgeniev.github.io/netsuite/callback', // Callback URL after authentication
   };
@@ -30,8 +32,6 @@ export class AuthService {
     this.oauthService.configure(this.authConfig);
     this.oauthService.setupAutomaticSilentRefresh();
     this.oauthService.checkOrigin = false;
-    this.oauthService.tryLogin();
-    
   }
 
   login() {
